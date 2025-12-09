be4c0fabbe9efac025adcd3343d1f2380658cb9a
Amit-bott <ak6013348@gmail.com>
2025-12-09 23:11:17 +0530

    Update world-population-analysis.py

--- world-population-analysis.py
+++ world-population-analysis.py
@@ -6,9 +6,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import os
 
-# -------------------------------
-# CONFIG
-# -------------------------------
+
 st.set_page_config(
     page_title="World Data Dashboard",
     page_icon="🌍",
